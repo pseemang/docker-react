b6199db4da166a269c9dc550b01437229dac42fc
Palan <pseemangal@gmail.com>
2020-10-26 17:33:48 -0400

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn reactt/i);
   expect(linkElement).toBeInTheDocument();
 });
